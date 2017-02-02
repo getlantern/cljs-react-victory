b68d905942d664572ff999fb700c79959bce2485
Ulysses Aalto <uaalto@getlantern.org>
2017-02-02 19:09:00 +0100

    Animation example

--- src/cljs_react_victory/experiments.cljs
+++ src/cljs_react_victory/experiments.cljs
@@ -21,43 +21,81 @@
             {:quarter 3 :earnings 10250}
             {:quarter 4 :earnings 25000}])
 
+(def data-error [{:x 15 :y 35000 :error 0.2}
+                 {:x 20 :y 42000 :error 0.05}
+                 {:x 30 :y 55000 :error 0.1}])
+
+
 (js/console.log data)
 
-(defn test-component []
-  [:div {:style {:text-align "center"}}
+(defn bars-component []
+  [:div
    [:h2 "Bar components"]
    [:h4 "Bars Simple"]
-   [:div {:style {:width "500px" :margin "auto"}}
-    ;; Bars Simple
-    [v/chart {;; domainPadding will add space to each side of VictoryBar to
-              ;; prevent it from overlapping the axis
-              :domainPadding 20
-              :theme v/material-theme}
-     [v/axis {;; tickValues specifies both the number of ticks and where
-              ;; they are placed on the axis
-              :tickValues ["Quarter 1" "Quarter 2" "Quarter 3" "Quarter 4"]}]
-     [v/axis {:dependentAxis true
-              ;; tickFormat specifies how ticks should be displayed
-              :tickFormat #(str "$ " (/ % 1000) "k")}]
+   ;; Bars Simple
+   [v/chart {;; domainPadding will add space to each side of VictoryBar to
+             ;; prevent it from overlapping the axis
+             :domainPadding 20
+             :theme v/material-theme}
+    [v/axis {;; tickValues specifies both the number of ticks and where
+             ;; they are placed on the axis
+             :tickValues ["Quarter 1" "Quarter 2" "Quarter 3" "Quarter 4"]}]
+    [v/axis {:dependentAxis true
+             ;; tickFormat specifies how ticks should be displayed
+             :tickFormat #(str "$ " (/ % 1000) "k")}]
+    [v/bar {:data data
+            :x :quarter
+            :y :earnings}]]
+   ;; Multiple Bars
+   [:h4 "Bars Multiple"]
+   [v/chart {:domainPadding 20
+             :theme v/material-theme}
+    [v/axis {;; tickValues specifies both the number of ticks and where
+             ;; they are placed on the axis
+             :tickValues ["Gold" "Silver" "Titanium" "Ore"]}]
+    [v/axis {:dependentAxis true
+             ;; tickFormat specifies how ticks should be displayed
+             :tickFormat #(str "$ " (/ % 1000) "M")}]
+    [v/stack {:colorScale "warm"}
      [v/bar {:data data
              :x :quarter
-             :y :earnings}]]
-    ;; Multiple Bars
-    [:h4 "Bars Multiple"]
-    [v/chart {:domainPadding 20
-              :theme v/material-theme}
-     [v/axis {;; tickValues specifies both the number of ticks and where
-              ;; they are placed on the axis
-              :tickValues ["Gold" "Silver" "Titanium" "Ore"]}]
-     [v/axis {:dependentAxis true
-              ;; tickFormat specifies how ticks should be displayed
-              :tickFormat #(str "$ " (/ % 1000) "M")}]
-     [v/stack {:colorScale "warm"}
-      [v/bar {:data data
-              :x :quarter
-              :y :earnings}]
-      [v/bar {:data data2
-              :x :quarter
-              :y :earnings}]]]]])
+             :y :earnings}]
+     [v/bar {:data data2
+             :x :quarter
+             :y :earnings}]]]])
 
-(r/render-component [test-component] (js/document.getElementById "app"))
+(defn error-bars-component []
+  [:div
+   [:h2 "Error Bars"]
+   [:h4 {:style {:color "#f00"}} "Error Bars Simple (doesn't work)"]
+   ;; Error bars
+   [v/chart
+    [v/error-bar {:data data-error
+                  :errorX #(* (:error %) (:x %))
+                  :errorY #(* (:error %) (:y %))}]]])
+
+(defn scatter-component []
+  (let [generate-data (fn []
+                        (take 20 (repeatedly (fn [] {:x (rand-int 10)
+                                                     :y (rand-int 20)
+                                                     :size (rand-int 20)}))))
+        scatter-data (r/atom (generate-data))
+]
+    (js/setTimeout #(reset! scatter-data (generate-data)) 4000)
+    (fn []
+      [:div
+       [:h2 "Scatter Data"]
+       [:h4 "Scatter Data (ANIMATED)"]
+       [v/chart {:theme v/material-theme
+                 :animate {:duration 1000
+                           :easing "circleOut"}}
+        [v/scatter {:data @scatter-data}]]])))
+
+(defn main-component []
+  [:div {:style {:text-align "center"}}
+   [:div {:style {:width "500px" :margin "auto"}}
+    (bars-component)
+    (error-bars-component)
+    ((scatter-component))]])
+
+(r/render-component [main-component] (js/document.getElementById "app"))
--- src/cljs_react_victory/reagent.cljs
+++ src/cljs_react_victory/reagent.cljs
@@ -3,13 +3,19 @@
             [reagent.core :as r]
             [camel-snake-kebab.core :as cs :include-macros true]))
 
+(def area (r/adapt-react-class js/Victory.VictoryArea))
 (def axis (r/adapt-react-class js/Victory.VictoryAxis))
 (def bar (r/adapt-react-class js/Victory.VictoryBar))
+(def candlestick (r/adapt-react-class js/Victory.VictoryCandlestick))
 (def chart (r/adapt-react-class js/Victory.VictoryChart))
+(def error-bar (r/adapt-react-class js/Victory.VictoryErrorBar))
 (def group (r/adapt-react-class js/Victory.VictoryGroup))
 (def line (r/adapt-react-class js/Victory.VictoryLine))
 (def pie (r/adapt-react-class js/Victory.VictoryPie))
+(def scatter (r/adapt-react-class js/Victory.VictoryScatter))
 (def stack (r/adapt-react-class js/Victory.VictoryStack))
+(def voronoi (r/adapt-react-class js/Victory.VictoryVoronoi))
+(def voronoi-tooltip (r/adapt-react-class js/Victory.VictoryVoronoiTooltip))
 (def zoom (r/adapt-react-class js/Victory.VictoryZoom))
 
 (def material-theme js/Victory.VictoryTheme.material)
